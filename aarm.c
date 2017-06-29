6108c9aa206ca6cda976d1e99ae387353d27ea61
Soundarya0605 <soundaryamurugan1997@gmail.com>
2017-06-29 12:40:06 +0530

    Create aarm.c

---
+++ aarm.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#include <math.h>
+
+int main()
+
+{
+
+    int number,count=0,sum = 0, rem = 0, cube = 0,i, temp;
+
+    scanf("%d", &number);
+
+    temp = number;
+
+    while (number != 0)
+
+    {
+
+        rem = number % 10;
+
+        cube = pow(rem, 4);
+
+        sum = sum + cube;
+
+        number = number / 10;
+
+    }
+    
+
+    if (sum == temp)
+
+        printf ("The given no  %d is armstrong no",temp);
+
+    else
+
+        printf ("The given no %d is not a armstrong no",temp);
+    return 0;
+
+}
